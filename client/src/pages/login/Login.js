15b7789339a4c29bbad0085b82f2b3040411fd1d
marcivallario <68357009+marcivallario@users.noreply.github.com>
2022-07-28 17:59:03 -0400

    Basic CSS for Login
    - add base logic for password showing

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,16 +1,23 @@
+import { useState } from "react";
+import "./login.css"
+
 function Login() {
+    const [ showInput, setShowInput ] = useState(false);
+
     return (
         <div className="container-fluid" id="login">
-            <div className="container login-container">
-                <h1>Welcome to Yum Eutopia!</h1>
-                <p>Throw in your credentials and let's get cookin'.</p>
-                <form>
-                    <input type="text" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <p><a href="http://www.google.com">Forgot password?</a></p>
-                    <input type="submit" value="Sign in"/>
-                </form>
-                <p>Not a member? Get started <a href="http://www.google.com">here</a>.</p>
+            <div className="login-container">
+                <div className="login-content">
+                    <h1>Welcome back!</h1>
+                    <p>Throw in your credentials and let's get cookin'.</p>
+                    <form>
+                        <input type="text" placeholder="Email" />
+                        <input type={showInput ? 'text' : 'password'} placeholder="Password" />
+                        <p className="pass-reset"><a href="http://www.google.com">Forgot password?</a></p>
+                        <input type="submit" value="Sign in"/>
+                    </form>
+                    <p className="signup-call">Not a member? Get started <a href="http://www.google.com">here</a>.</p>
+                </div>
             </div>
         </div>
     )
